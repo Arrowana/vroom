a2cec30b018fa7756cf6b0053d76dacc2255aeb7
Arrowana <pierre.duval@gadz.org>
2016-11-09 23:17:46 +1100

    improve valid check and use vertex object for vertices

--- citygenerator.py
+++ citygenerator.py
@@ -10,16 +10,29 @@ class Point:
     def __str__(self):
         return 'Point class ({}, {})'.format(self.x, self.y)
 
-class Vertex:
-    def __init__(self, point):
+class Vertex():
+    def __init__(self, point, neighbours = None):
         self.point = point 
-        self.neighboors = []
+        
+        if neighbours:
+            self.neighbours = neighbours
+        else:
+            self.neighbours = []
+
+    def __str__(self):
+        return 'Vertex ({}, {})'.format(self.point[0], self.point[1])
+
+    def __repr__(self):
+        return '<Vertex point:({}, {})>'.format(self.point[0], self.point[1])
+
+    def get_previous_vertex(self):
+        return self.neighbours[-1]
 
 def vector(point_a, point_b):
     """Vector AB"""
     return point_b[0] - point_a[0], point_b[1] - point_a[1]
 
-def vector_normal(point_a, point_b):
+def vector_normalized(point_a, point_b):
     vec = vector(point_a, point_b)
     norm = math.sqrt(vec[0]**2+vec[1]**2)
 
@@ -32,29 +45,85 @@ def get_random_vector(vector, l_min, l_max):
 def norm(a, b):
     return math.sqrt((a[0]-b[0])**2+(a[1]-b[1])**2)
 
-def candidate_validation(candidate, segments):
-    """Check if the candidate is valid"""
-    distance_min = 0.5 
+def dot(a, b):
+    return a[0]*b[0] + a[1]*b[1]
 
-    for pt1, pt2 in segments:
-        if norm(candidate, pt1) < distance_min:
-            candidate = pt1
-            break
-        if norm(candidate, pt2) < distance_min:
-            candidate = pt2
-            break
+def vector_minus(a, b):
+    return a[0]-b[0], a[1]-b[1]
+
+def line_param(a, b, t):
+    return a[0] + t*b[0], a[1] + t*b[1]
+
+def intersection(p, r, q, s):
+    # p + t*q first line
+    # r + u*s second line
+    #source http://stackoverflow.com/a/565282
+
+    u_num = dot(vector_minus(q, p), r)
+    u_den = dot(r, s)
+
+    if abs(u_den) < 0.001 and abs(u_num) < 0.001:
+        # Colinear
+        return None
+    elif abs(u_den) < 0.001 and abs(u_num) >= 0.001:
+        # Parallel
+        return None
+
+    t_num = dot(vector_minus(q, p), s)
+    t = t_num/u_den
+    u = u_num/u_den, u_num/u_den
+
+    if abs(dot(r, s)) >= 0.001 and 0 < t and t < 1 and 0 < u and u < 1:
+        #Two segment intersects
+        return line_param(p, q, t)
+    else:
+        return None
+
+
+def candidate_validation(candidate, previous_vertex, vertices):
+    """Check if the candidate is valid, return a candidate vertex or None"""
+    distance_min = 3 
+
+
+    #Check if the vertex is intersecting any segment
+    # Implemented if it crosses one line only
+    for vertex in vertices:
+        for neighbour in vertex.neighbours:
+            inter = intersection(previous_vertex.point, candidate, vertex.point, neighbour.point)
+            if inter:
+                print 'Inter'
+                candidate = inter
+
+                vertex_candidate = Vertex(candidate, neighbours=[vertex, neighbour])
+
+                vertex.neighbours.remove(neightbour)
+                vertex.neighbours.append(vertex_candidate)
+
+                neightbour.neightbours.remove(vertex)
+                neightbour.neighbours.append(vertex_candidate)
+
+                return vertex_candidate
+
+    # check for points nearby and replace the candidate with it
+    for vertex in vertices:
+        if norm(candidate, vertex.point) < distance_min:
+            vertex_candidate = vertex
+            print 'link to existing intersection'
+            return vertex_candidate
 
     print 'val'
-    return candidate
+    vertex_candidate = Vertex(candidate)
+    return vertex_candidate
 
-def grid_growth(previous_point, current_point):
+def grid_growth(vertex):
     p_forward = 0.8 #Probability of adding a point forward
     p_turn = 0.6 # Probability of adding a turn
 
     l_min = 5
     l_max = 10
 
-    previous_vector = vector_normal(previous_point, current_point)
+    previous_point = vertex.point
+    previous_vector = vector_normalized(vertex.get_previous_vertex().point, vertex.point)
 
     n = -previous_vector[1], previous_vector[0] #Normal to previous segment
     candidates = []
@@ -62,43 +131,54 @@ def grid_growth(previous_point, current_point):
     if random.random() <= p_forward:
         #Go straight 
         v = get_random_vector(previous_vector, l_min, l_max)
-        new_point = current_point[0] + v[0], current_point[1] + v[1]
+        new_point = previous_point[0] + v[0], previous_point[1] + v[1]
         candidates.append(new_point)
 
     if random.random() <= p_turn:
         n_scaled = get_random_vector(n, l_min, l_max)
-        new_point = current_point[0] + n_scaled[0], current_point[1]+n_scaled[1]
+        new_point = previous_point[0] + n_scaled[0], previous_point[1]+n_scaled[1]
         candidates.append(new_point)
 
     if random.random() <= p_turn:
         n_scaled = get_random_vector(n, l_min, l_max)
-        new_point = current_point[0] - n_scaled[0], current_point[1]-n_scaled[1]
+        new_point = previous_point[0] - n_scaled[0], previous_point[1]-n_scaled[1]
         candidates.append(new_point)
 
     return candidates
 
+def test_grid_growth():
+    pass
+
 def generate():
-    seed = [[0,0], [10,0]]
-    segments = []
-    queue = [seed] # queue of segments
+    start = Vertex([0,0])
+    second_point = Vertex([10, 0], neighbours=[start])
+    start.neighbours = [second_point]
+
+    vertices = [start, second_point] #All generated and valid vertices
+    queue = [second_point] # queue of vertices
 
     for i in range(500):
-        previous_segment = queue[-1]
-        candidates = grid_growth(*previous_segment)
-        print(candidates)
+        previous_vertex = queue[-1]
+        print 'previous_vertex:', previous_vertex
+        candidates = grid_growth(previous_vertex)
+        print 'candidates:', candidates
 
         for candidate in candidates:
-            #No check for now
-            candidate = candidate_validation(candidate, segments)
+            vertex_candidate = candidate_validation(candidate, previous_vertex, vertices)
 
-            new_segment = [previous_segment[-1], candidate]
-            print(new_segment)
-            queue.append(new_segment)
-            segments.append(new_segment)
+            if vertex_candidate in vertices:
+                print 'Already in'
+                vertex_candidate.neighbours.append(previous_vertex)
+            else:
+                vertex_candidate.neighbours.append(previous_vertex)
+                queue.append(vertex_candidate)
+                vertices.append(vertex_candidate)
 
-    print(segments)
-    for segment in segments:
-        plt.plot(*zip(*segment))
+    print vertices[:10]
+
+    for vertex in vertices:
+        for neighbour in vertex.neighbours:
+            plt.plot(*zip(*[vertex.point, neighbour.point]))
 
     plt.show()
         
