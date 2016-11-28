2996f441155c5aebe94b39effea49961f7abe8ed
Arrowana <pierre.duval@gadz.org>
2016-11-28 17:42:39 +1100

    basic test

--- server/client.py
+++ server/client.py
@@ -1,5 +1,6 @@
 import socket
 import time
+import re
 
 import struct
 
@@ -18,16 +19,29 @@ s.connect((address, port))
 
 previous_time = time.time()
 
-try:
-    s.send('name:{}'.format(name))
-    while True:
-        time.sleep(0.05) #test sending state every 50 ms
-        data = s.recv(1024)
-        print data
+userid = None
 
-        if time.time() - previous_time > 0.05:
-            #send our state
-            s.send('10')
-            previous_time = time.time()
+def handshake():
+	s.send('get_id'.format(name))
+	time.sleep(0.05) #test sending state every 50 ms
+	data = s.recv(1024)
+	print data
+	matched = re.match(r'id:(\d+)', data)
+	if matched:
+		userid = matched.groups(1)
+	else:
+		raise ValueError('{} is not a id response'.format(data))
+
+try:
+	handshake()
+
+	while True:
+		data = s.recv(1024)
+		print data
+		if time.time() - previous_time > 0.05:
+			if userid:
+				#send our state
+				s.send('10')
+				previous_time = time.time()
 finally:
-    s.close()
+	s.close()
--- server/socket_server.py
+++ server/socket_server.py
@@ -18,69 +18,74 @@ serversocket.listen(5)
 read_list = [serversocket]
 write_list = []
 
-queue = ['10']*10 #FIFO queue for data
 clients = {}
 
-FORMAT = '>h2f'
+FORMAT = '>h3f'
+
+def package(userid, x, y, heading):
+	return struct.pack(FORMAT, userid, x, y, heading)
 
 def send():
-    clientsocket.send(struct.pack('>2f', *[random.random() for i in range(2)]))
+	clientsocket.send(struct.pack('>2f', *[random.random() for i in range(2)]))
 
 class Client:
-    def __init__(self):
-        self.username = None
-        self.state = None
-    
-    def set_username(self, username):
-        self.username = username
+	def __init__(self):
+		self.username = None
+		self.state = None
+		self.userid = None
+	
+	def set_username(self, username):
+		self.username = username
 
 class GameState:
-    def __init__(self):
-        pass
+	def __init__(self):
+		pass
 
 previous_update = time.time()
+current_userid = 0
 
 while True:
-    readable, writable, errored = select.select(read_list, write_list, [])
-    for s in readable:
-        if s is serversocket:
-            clientsocket, address = serversocket.accept()
-            print 'Connection from :', address
+	readable, writable, errored = select.select(read_list, write_list, [])
+	for s in readable:
+		if s is serversocket:
+			clientsocket, address = serversocket.accept()
+			print 'Connection from :', address
 
-            write_list.append(clientsocket)
-            read_list.append(clientsocket)
-            clients[clientsocket] = Client()
-        else:
-            data = s.recv(1024)
-            if data:
-                print 'Received:', data
+			write_list.append(clientsocket)
+			read_list.append(clientsocket)
+			clients[clientsocket] = Client()
+		else:
+			data = s.recv(1024)
+			if data:
+				print 'Received:', data
 
-                matched = re.match(r'name:([\w\d]+)', data)
-                if matched:
-                    name = matched.group(1)
-                    print 'Received username from user:', name
-                    clients[s].set_username(name)
-                else:
-                    print 'Received state: {}, from: {}'.format(data, clients[s].username)
-                    clients[s].state = data
-                    
-    if time.time() - previous_update > 0.05:
-        #Update state
-        previous_update = time.time()
-        payload = ''
-        for s, client in clients.items():
-            payload += '{}:{},'.format(client.username, client.state)
-        print 'Send payload'
-    else:
-        payload = None
+				matched = re.match(r'get_id', data)
+				if matched:
+					print 'Received request for id from user:', s
+					clients[s].userid = current_userid
+					s.send('id:{}'.format(current_userid))
+					current_userid += 1
+				else:
+					print 'Received state: {}, from: {}'.format(data, clients[s].username)
+					clients[s].state = data
+					
+	if time.time() - previous_update > 0.05:
+		#Update state
+		previous_update = time.time()
+		payload = ''
+		for s, client in clients.items():
+			payload += '{}:{},'.format(client.userid, client.state)
+		print 'Send payload'
+	else:
+		payload = None
 
-    if payload:
-        for w in writable:
-            #Broadcast the state
-            try:
-                w.send(payload)
-            except socket.error:
-                read_list.remove(w)
-                write_list.remove(w)
+	if payload:
+		for w in writable:
+			#Broadcast the state
+			try:
+				w.send(payload)
+			except socket.error:
+				read_list.remove(w)
+				write_list.remove(w)
 
-    time.sleep(0.005)
+	time.sleep(0.005)
